a521913c54f5e429edefa99b44a1d97ab9f39450
Joshua Sullivan <joshuasullivan0211@gmail.com>
2018-05-15 22:39:35 -0500

    Miscellaneous commit to keep progress
    Lots of updates to UI and minor code updates.

--- app/src/main/java/com/example/pushpull/game_logic/LevelManager.java
+++ app/src/main/java/com/example/pushpull/game_logic/LevelManager.java
@@ -1,8 +1,11 @@
 package com.example.pushpull.game_logic;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.widget.TextView;
 
+import com.example.pushpull.R;
 import com.example.pushpull.myLibrary.Vector2D;
 
 import java.io.BufferedReader;
@@ -10,7 +13,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Map;
 
 
 public class LevelManager {
@@ -18,31 +20,32 @@ public class LevelManager {
 
     private Level currentLevel;
     private int levelIndex = 0;
-    private String[] filePaths;
+    private String[] filePaths = new String[0];
     private Context context;
+    private AssetManager assetManager;
 
-    private int levelCount = 0;
 
 
-    public LevelManager(Context context) {
 
+
+    public LevelManager(Context context) throws IOException {
 
         this.context = context;
-        currentLevel = createLevelFromIndex(0);
-
-        if (filePaths != null) {
-            levelCount = filePaths.length;
+        try {
+            assetManager = context.getAssets();
+            filePaths = assetManager.list("Levels");
+        }
+        catch (IOException e) {
+            //TODO: handle IOException
+            e.printStackTrace();
         }
 
     }
 
-    private Level createLevelFromIndex(int index) {
+    private Level createLevelFromIndex(int index) throws LevelLoadException {
 
         Level result;
         try {
-
-            AssetManager assetManager = context.getAssets();
-            filePaths = assetManager.list("Levels");
             InputStream singleLevel = assetManager.open("Levels/" + filePaths[index]);
             BufferedReader br = new BufferedReader(new InputStreamReader(singleLevel));
 
@@ -57,17 +60,16 @@ public class LevelManager {
             return result;
         }
         catch (IOException e) {
-            e.printStackTrace();
-            return null;
+            throw new LevelLoadException("Error when attempting to open file.");
         }
     }
 
 
     public int getLevelCount() {
-        return levelCount;
+       return filePaths.length;
     }
 
-    public boolean checkVictory() {
+    public boolean checkVictory() throws LevelLoadException {
         if (currentLevel.isComplete()) {
             advanceLevel();
             return true;
@@ -81,13 +83,40 @@ public class LevelManager {
         return currentLevel.getSerialState();
     }
 
-    public void advanceLevel() {
+    public void advanceLevel() throws LevelLoadException{
         setLevel(levelIndex + 1);
     }
 
-    public void setLevel(int levelIndex) {
+    public void setLevel(int levelIndex) throws LevelLoadException{
         this.levelIndex = levelIndex;
         this.currentLevel = createLevelFromIndex(levelIndex);
+        setMessage();
+
+    }
+
+    private void setMessage() {
+        TextView message = ((Activity) context).findViewById(R.id.message);
+        String text = "";
+        switch(levelIndex) {
+            case 0:
+                text = "Swipe to move.";
+                break;
+            case 1:
+                text = "You must cover all of the pink circles to win.";
+                break;
+            case 2:
+                text = "When a square is blue, it can pull but not push.";
+                break;
+            case 3:
+                text = "Move over the red/blue circles to change color.";
+                break;
+            case 9:
+                text = "Blocks can be larger and different shapes, but still can be moved.";
+                break;
+            case 11:
+                text = "A yellow square moves all connected blocks.";
+        }
+        message.setText(text);
     }
 
     public Level getCurrentLevel() {
@@ -102,7 +131,7 @@ public class LevelManager {
         currentLevel.revertState();
     }
 
-    public void reset() {
+    public void reset() throws LevelLoadException {
         currentLevel = new Level(currentLevel.getLayout());
     }
 }
